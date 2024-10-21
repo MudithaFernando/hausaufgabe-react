b64318f2cb62c2c804d3744145f96b78c42d03e1
muditha <mudithafernando28@gmail.com>
2024-10-21 14:57:34 +0200

    Aufgabe21-10

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  // Definiere eine Begrüßungsvariable
+  const greeting = "Heute ist ein großartiger Tag, React zu lernen!";
+
+  // Eine einfache Berechnung
+  const sum = 5 + 3;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Willkommen zu meiner ersten React-App</h1>
+      <h2>{greeting}</h2>
+      <h3>Die Summe von 5 + 3 ist: {sum}</h3>
+      <button onClick={() => alert('Hallo, dies ist ein Alert!')}>Klick mich!</button>
+    </>
   );
 }
 
